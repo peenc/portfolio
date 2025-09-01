2a9b1f333b076a65da477db31547f87e8df44326
Pedro <pedro_cardoso@ufrrj.br>
2025-09-01 17:01:52 -0300

    Atualização Sobre

--- src/jsx/Home.jsx
+++ src/jsx/Home.jsx
@@ -40,11 +40,9 @@ export default function Home() {
         <div className="home-container">
             <Navbar />
 
-            {/* wrapper para o conteúdo principal */}
             <div className="main-content">
                 <Header />
 
-                {/* Seções informativas */}
                 <InfoSection id="sobre" title="Sobre Mim">
                     <p>
                         Sou estudante de Ciência da Computação na UFRRJ, com forte interesse em desenvolvimento back-end, arquitetura de sistemas e boas práticas de engenharia de software.
@@ -61,6 +59,38 @@ export default function Home() {
                             <em>Previsão de conclusão: 2026</em>
                         </li>
                         <li><strong>Ensino Médio</strong> – CIEP 175 – José Lins do Rego (2017)</li>
+                    </ul>
+
+                    <h3 style={{ marginTop: '20px' }}>Conhecimentos Técnicos</h3>
+                    <ul>
+                        <li>
+                            <strong>Linguagens:</strong> Java (avançado), Python, C, C++, JavaScript, C#.
+                        </li>
+                        <li>
+                            <strong>Back-end:</strong> Spring, Spring Security, Spring JPA, Spring Web, REST APIs
+                        </li>
+                        <li>
+                            <strong>Front-end:</strong> React, Thymeleaf, HTML, CSS, Bootstrap, Tailwind CSS.
+                        </li>
+                        <li>
+                            <strong>Banco de Dados:</strong> MySQL, PostgreSQL, H2, MongoDB.
+                        </li>
+                        <li>
+                            <strong>Integrações e APIs:</strong> Pix API, QR Code, MapLibre, API banco do Brasil, API Riot Games.
+                        </li>
+                        <li>
+                            <strong>Ferramentas e IDEs:</strong> Junit, Git, GitHub, Postman, IntelliJ, Eclipse, VSCode, Jira, Trello.
+                        </li>
+                        <li>
+                            <strong>Outros:</strong> Lógica de programação, MVC, Microsserviços, Godot, Ren’Py.
+                        </li>
+                    </ul>
+
+
+                    <h3 style={{ marginTop: '20px' }}>Cursos Complementares</h3>
+                    <ul>
+                        <li>Java Orientado a Objetos – Udemy (2023)</li>
+                        <li>Assistente Administrativo – SEST SENAT (2018–2019)</li>
                     </ul>
                 </InfoSection>
 
